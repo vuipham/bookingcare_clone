27f596d2041bcb72c9f6c3b3597b609eedf2a21f
Sadnot <92632621+Sadnot@users.noreply.github.com>
2022-12-05 14:13:19 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,18 @@
+import express from "express";
+import homeController from "../controllers/homeController"
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('/', homeController.getHomePage);
+
+    router.get('/trangchu', (req, res) => {
+        return res.send("trang chu")
+    });  
+
+    //rest api
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
